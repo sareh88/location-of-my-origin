a3adecccdce9306ca4d2f369b1d7cadf00ef19eb
yassermorteza <yasser.morteza@gmail.com>
2018-02-15 13:33:08 +0100

    user model

---
+++ models/user.model.js
@@ -0,0 +1,41 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	email: {
+    	type: String,
+    	required: true,
+    	unique: true
+    } ,
+    name: {
+    	type: String,
+    	required: true
+    } ,
+    city: {
+    	type: String,
+    	required: true
+    },
+    lang: {
+    	type: String,
+    	required: true
+    },
+    lot: {
+    	type: String,
+    	required: true
+    },
+    flag: {
+    	type: String,
+    	required: true
+    },
+    info: {
+    	type: String,
+    	required: true
+    },
+    image: {
+    	type: String,
+    	required: true
+    }
+});
+
+module.exports = mongoose.model('user', userSchema);
