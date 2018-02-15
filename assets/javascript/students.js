fadfff20023020673ecf49ef7afd93e1dbb0ec26
Sash Seurat Samson <sash.samson@gmail.com>
2018-02-15 14:09:14 +0100

    index changing

---
+++ assets/javascript/students.js
@@ -0,0 +1,37 @@
+[{
+    "name": "Martha",
+    "city": "Firenze",
+    "lat": 43.805909,
+    "long": 11.191361,
+    "info": "Born in september 26 1989, Florence - Italy. I love the city, but many many tourists, I miss the food, but I prefer being in Berlin. always happy to go to my hometown, and actually I appreciate it more since i am living in berlin. My parents live there",
+    "user-image": "https://static1.squarespace.com/static/5a18c99380bd5e0fc6d7c516/5a3eda6f53450ae78e99ad0e/5a4274458165f5d70b9f4325/1514304594121/Gerry+Jacket+1.jpg",
+    "flag": "http://3.bp.blogspot.com/-ZlLjoWInvVE/UHfPbXWcwII/AAAAAAAAAPM/mPTILGhAmvE/s1600/printable_flag_of_italy.png"
+  },
+  {
+    "name": "Sash",
+    "city": "Tel Aviv",
+    "lat": 32.0677778,
+    "long": 34.7647222,
+    "info": "From Jaffa-Tel Aviv, a beachtown with great food & nightlife.",
+    "user-image": "https://openclipart.org/image/800px/svg_to_png/23752/Anonymous-Italian-peace-flag-1.png",
+    "flag": ""
+  },
+  {
+    "name": "Yasser Morteza",
+    "city": "Gorgan",
+    "lat": 36.8502819,
+    "long": 54.3678513,
+    "info": "",
+    "user-image": "https://openclipart.org/image/800px/svg_to_png/23752/Anonymous-Italian-peace-flag-1.png",
+    "flag": ""
+  },
+  {
+    "name": "Meralle Halabian",
+    "city": "Aleppo",
+    "lat": 36.8502819,
+    "long": 54.3678513,
+    "info": "",
+    "user-image": "https://openclipart.org/image/800px/svg_to_png/23752/Anonymous-Italian-peace-flag-1.png",
+    "flag": ""
+  }
+  ]
